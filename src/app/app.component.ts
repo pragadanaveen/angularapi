a6c499b91ca7d5ecd592b058d2f09b5da67c87dd
pragadanaveen <naveenpragada99@gmail.com>
2024-02-10 15:00:31 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PostService } from './services/post.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'proser';
+
+
+  posts:any;
+  
+    constructor(private service:PostService) {}
+  
+    ngOnInit() {
+  
+      this.service.getPosts()
+        .subscribe(response => {
+          this.posts = response;
+        });
+  }
 }
